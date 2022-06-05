262af75e821a52dffa6307b16a20dc84a7a444fc
Javier Duarte <jduarte@ucsd.edu>
2022-06-05 15:32:27 -0500

    rm commented code

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -16,11 +16,3 @@ provide_dockerfile = '{{cookiecutter.provide_dockerfile}}' == 'yes'
 if not provide_dockerfile:
     # remove top-level file inside the generated folder
     remove('Dockerfile')
-
-# if not create_file_one:
-#     # remove absolute path to file nested inside the generated folder
-#     remove(os.path.join(os.getcwd(), cookiecutter.package_name, 'file_one.py'))
-
-# if not create_file_two:
-#     # remove relative file nested inside the generated folder
-#     remove(os.path.join(cookiecutter.package_name, 'file_two.py'))
